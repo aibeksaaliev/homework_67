cab42f6af10e55cd067257fdefb5d3b15b943862
Aibek Saaliev <dokaidea@gmail.com>
2022-12-17 16:47:15 +0600

    removed bootstrap import from index

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import App from './App';
