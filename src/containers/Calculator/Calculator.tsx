654438bad2eca56305d86baca2b6de2aa9bd4d88
Aibek Saaliev <dokaidea@gmail.com>
2022-12-17 16:45:00 +0600

    done main functional of calculator

--- src/containers/Calculator/Calculator.tsx
+++ src/containers/Calculator/Calculator.tsx
@@ -16,27 +16,22 @@ const Calculator = () => {
           <div className="equation">{calculation}</div>
           <div className="result">{result}</div>
         </div>
-
         <div className="key highlight" onClick={() => dispatch(clearMemory())}>AC</div>
         <div className="key highlight">±</div>
         <div className="key highlight">%</div>
         <div className="key highlight" onClick={() => dispatch(operation("/"))}>/</div>
-
         <div className="key" onClick={() => dispatch(addNumber("7"))}>7</div>
         <div className="key" onClick={() => dispatch(addNumber("8"))}>8</div>
         <div className="key" onClick={() => dispatch(addNumber("9"))}>9</div>
         <div className="key highlight" onClick={() => dispatch(operation("*"))}>*</div>
-
         <div className="key" onClick={() => dispatch(addNumber("4"))}>4</div>
         <div className="key" onClick={() => dispatch(addNumber("5"))}>5</div>
         <div className="key" onClick={() => dispatch(addNumber("6"))}>6</div>
         <div className="key highlight" onClick={() => dispatch(operation("-"))}>-</div>
-
         <div className="key" onClick={() => dispatch(addNumber("1"))}>1</div>
         <div className="key" onClick={() => dispatch(addNumber("2"))}>2</div>
         <div className="key" onClick={() => dispatch(addNumber("3"))}>3</div>
         <div className="key highlight" onClick={() => dispatch(operation("+"))}>+</div>
-
         <div className="key"  onClick={() => dispatch(addNumber("."))}>.</div>
         <div className="key" onClick={() => dispatch(addNumber("0"))}>0</div>
         <div className="key" onClick={() => dispatch(deleteNumber())}>&#9003;</div>
